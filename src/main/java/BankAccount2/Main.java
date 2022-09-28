fdcdf3fe6a184a70755ce131e42fd005b34d38c1
Daren Shamoun <darenshamoun@gmail.com>
2022-09-28 01:55:14 -0700

    Fixed typos throughout the project and cleaned the style to professors specifications, fixed bug on line 128 to allow for custom inputs, refactored some variables so that they make more sense in context.

--- src/main/java/BankAccount2/Main.java
+++ src/main/java/BankAccount2/Main.java
@@ -4,8 +4,10 @@ import java.util.Random;
 import java.util.Scanner;
 
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         int[][] preBuiltOptions = new int[2][2];
         preBuiltOptions[0][0]= 100;
         preBuiltOptions[0][1]=1000;
@@ -14,58 +16,51 @@ public class Main {
 
         User user = new User("1","1","1");
 
-
-
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Plese enter your info to login\n" +
-                "Hint: Key senstive");
-        System.out.println("Plese enter your user name");
-        String userName= scanner.nextLine();
-        System.out.println("Please enter your lastName");
-        String userGender = scanner.nextLine();
-        System.out.println("Please enter your email");
+        System.out.println("Please enter the following information to login\n" +
+                "Hint: Case sensitive");
+        System.out.println("Please enter your fist name");
+        String firstName = scanner.nextLine();
+        System.out.println("Please enter your last name");
+        String lastName = scanner.nextLine();
+        System.out.println("Please enter your email address");
         String userEmail = scanner.nextLine();
 
 
-
-        if (userName.equals(user.getName())&&userGender.equals(user.getLastName())&&userEmail.equals(user.getEmail())) {
-
+        if (firstName.equals(user.getName())&&lastName.equals(user.getLastName())&&userEmail.equals(user.getEmail()))
+        {
             loginText();
             System.out.println("Hello "+ user.getName()+"!");
 
-
-
-            int randomeBalance;
+            int randomBalance;
             Random moneyInAccount = new Random();
 
-            randomeBalance= moneyInAccount.nextInt(50000);
+            randomBalance = moneyInAccount.nextInt(50000);
 
-
-            double withDraw=0;
+            double withdraw = 0;
 
             double deposit = 0;
 
-            double userinfo[] ={randomeBalance,deposit,withDraw};
-
+            double[] userInfo = {randomBalance,deposit,withdraw };
 
             boolean logOut=false;
 
-            System.out.println("currentBalance: " +userinfo[0]);
-            while (!logOut) {
-
+            System.out.println("currentBalance: " + userInfo[0]);
+            while (!logOut)
+            {
                 infoText();
-
 
                 int userAction = scanner.nextInt();
                 scanner.nextLine();
 
-                switch (userAction) {
+                switch (userAction)
+                {
                     case 0:
                         System.out.println("Logging out...");
-                        logOut = true;
-                        break;
 
+                        logOut = true;
+
+                        break;
 
                     case 1:
 
@@ -74,33 +69,32 @@ public class Main {
                                 "press 2 for 1000$\n" +
                                 "Press 3 for custom ");
 
-
-                        int userChoiceNum1=scanner.nextInt();
+                        int userChoiceNum1 = scanner.nextInt();
                         scanner.nextLine();
-                        if (userChoiceNum1==1) {
-                            userinfo[0]+=preBuiltOptions[0][0];
-                            System.out.println("currentBalance: " + userinfo[0]);
+
+                        if (userChoiceNum1 == 1)
+                        {
+                            userInfo[0] += preBuiltOptions[0][0];
+                            System.out.println("currentBalance: " + userInfo[0]);
+
                             break;
-
-
-
                         }
-                        else if (userChoiceNum1==2) {
-                            userinfo[0]+=preBuiltOptions[0][1];
-                            System.out.println("currentBalance: " + userinfo[0]);
+                        else if (userChoiceNum1 == 2)
+                        {
+                            userInfo[0]+=preBuiltOptions[0][1];
+                            System.out.println("currentBalance: " + userInfo[0]);
+
                             break;
-
-
-
                         }
-                        else if (userChoiceNum1==3) {
-                            System.out.println("Plese enter amount!");
+                        else if (userChoiceNum1 == 3)
+                        {
+                            System.out.println("Please enter amount!");
                             double depositNum = scanner.nextDouble();
                             scanner.nextLine();
 
-                            userinfo[1] = depositNum;
-                            userinfo[0] += userinfo[1];
-                            System.out.println("currentBalance: " + userinfo[0]);
+                            userInfo[1] = depositNum;
+                            userInfo[0] += userInfo[1];
+                            System.out.println("currentBalance: " + userInfo[0]);
 
                             break;
                         }
@@ -115,63 +109,50 @@ public class Main {
                         int userChoiceNum2 = scanner.nextInt();
                         scanner.nextLine();
 
+                        if (userChoiceNum2==1)
+                        {
+                            userInfo[0]+=preBuiltOptions[1][0];
 
-                        if (userChoiceNum2==1) {
-                             userinfo[0]+=preBuiltOptions[1][0];
-
-                            System.out.println("currentBalance: " + userinfo[0]);
-                            break;
-
-
-                        }
-                        else if (userChoiceNum2==2) {
-                            userinfo[0]+=preBuiltOptions[1][1];
-
-                            System.out.println("currentBalance: " + userinfo[0]);
+                            System.out.println("currentBalance: " + userInfo[0]);
 
                             break;
-
-
                         }
-                        else if (userChoiceNum2==2) {
+                        else if (userChoiceNum2 == 2)
+                        {
+                            userInfo[0]+=preBuiltOptions[1][1];
 
-                            System.out.println("Plese enter amount!");
+                            System.out.println("currentBalance: " + userInfo[0]);
+
+                            break;
+                        }
+                        else if (userChoiceNum2 == 3)
+                        {
+                            System.out.println("Please enter amount!");
                             double withdrawNum = scanner.nextDouble();
                             scanner.nextLine();
-                            userinfo[2] = withdrawNum;
-                            userinfo[0] -= userinfo[2];
-                            System.out.println("currentBalance: " + userinfo[0]);
+                            userInfo[2] = withdrawNum;
+                            userInfo[0] -= userInfo[2];
+                            System.out.println("currentBalance: " + userInfo[0]);
 
                             break;
                         }
-
-
-
-
                 }
-
-
             }
         }
-        else {
+        else
+        {
             System.out.println("Login failed try again");
         }
-
-
-
     }
-    private static void loginText() {
-        System.out.println("Loging in...");
-
+    private static void loginText()
+    {
+        System.out.println("Logging in...");
     }
-    private static void infoText() {
-        System.out.println("\nPlese presss\n" +
+    private static void infoText()
+    {
+        System.out.println("\nPlease press\n" +
                 "0 to logout\n" +
                 "1 to add deposit\n" +
                 "2 to withdraw\n");
-
-
     }
-
-
 }
